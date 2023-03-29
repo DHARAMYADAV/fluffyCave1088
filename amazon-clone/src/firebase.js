2cd1488b14519677429a52320aa223a05a688c16
DHARAMYADAV <dharamysdav2.0@gmail.com>
2023-03-29 21:33:44 +0530

    code day-1

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ7uwFdlsGtLFkEc2fXBxAUz1KFR9VfDk",
+    authDomain: "clone-e33c1.firebaseapp.com",
+    projectId: "clone-e33c1",
+    storageBucket: "clone-e33c1.appspot.com",
+    messagingSenderId: "13253864815",
+    appId: "1:13253864815:web:2ede77affd251b67b2cc9a",
+    measurementId: "G-HBTGQ4VMKG"
+  };
